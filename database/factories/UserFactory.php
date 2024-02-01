3aaa798ddb9dd7d79c2d1b2a8c5abfa49a3f4834
Jérémy Gex <jeremy.gex@gmail.com>
2024-02-02 00:49:05 +0100

    Create user factory with seeder

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => fake()->unique()->safeEmail(),
+        ];
+    }
+}
